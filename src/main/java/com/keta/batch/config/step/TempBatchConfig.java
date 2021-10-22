16999299324d6f81eb2b9111bc270e9ec8644369
seunghwansuh <ssh32009757@gmail.com>
2021-10-22 17:01:22 +0900

    mybatis 연결하기

--- src/main/java/com/keta/batch/config/step/TempBatchConfig.java
+++ src/main/java/com/keta/batch/config/step/TempBatchConfig.java
@@ -1,7 +1,9 @@
-package com.keta.batch.job;
+package com.keta.batch.config.step;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -15,15 +17,19 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j // log 사용을 위한 lombok 어노테이션
 @RequiredArgsConstructor // 생성자 DI를 위한 lombok 어노테이션
 @Configuration
-public class JobConfiguration {
+public class TempBatchConfig {
     private final JobBuilderFactory jobBuilderFactory; // 생성자 DI 받음
     private final StepBuilderFactory stepBuilderFactory; // 생성자 DI 받음
+    private final SqlSessionFactory sqlSessionFactory;
+    private final SqlSessionTemplate sqlSessionTemplate;
+
+    @Value("${chunk.commit.unit:50}")
+    private int CHUNK_COUNT;
 
     @Bean
     public Job job() {
         return jobBuilderFactory.get("simpleJob")
                 .start(simpleStep1(null))
-                .next(simpleStep2(null))
                 .build();
     }
 
@@ -33,18 +39,6 @@ public class JobConfiguration {
         return stepBuilderFactory.get("simpleStep1")
                 .tasklet((contribution, chunkContext) -> {
                     log.info(">>>>> This is Step1");
-                    log.info(">>>>> requestDate = {}", requestDate);
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
-    }
-
-    @Bean
-    @JobScope
-    public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
-        return stepBuilderFactory.get("simpleStep2")
-                .tasklet((contribution, chunkContext) -> {
-                    log.info(">>>>> This is Step2");
                     log.info(">>>>> requestDate = {}", requestDate);
                     return RepeatStatus.FINISHED;
                 })
